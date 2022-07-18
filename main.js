79761a65f633a50e7c555433fd37566da263b680
Brenda Liu <brenda.liu24@gmail.com>
2022-07-18 16:30:07 -0700

    finish part one and create html for part two

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 $("img").addClass("image-center");
 $("p").eq(4).remove()
 $("#title").css("fontSize", "95px")
+
+$("aside").html($('<p>')).text('sorry for the list')
+
+
+function changeColor {
+  let $red = $('.form-control').eq(0).val();
+  let $blue = $('.form-control').eq(1).val();
+  let $green = $('.form-control').eq(2).val();
+  let $color = `rgb(${$red},${$blue},${$green})`;
+  $('body').css('backgroundColor',$color)
+}
+
+${body}.on('change',changeColor);
